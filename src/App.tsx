dd6de4ad29c60888d5d8b8d44a7509caaa09e03c
wallace <wallace_wolv@hotmail.com>
2024-10-13 12:56:09 -0300

    feat: Instalando React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './global.css'
 
-import { Button } from './components/ui/button'
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './routes'
 
 export function App() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <Button>Enviar</Button>
+      <RouterProvider router={router} />
     </div>
   )
 }
---
+++ src/pages/auth/sign-in.tsx
@@ -0,0 +1,7 @@
+export function SignIn() {
+  return (
+    <>
+      <h1>SignIn</h1>
+    </>
+  )
+}
