45dac519f9692ede759b426ecd3bd11028e6677c
wallace <wallace_wolv@hotmail.com>
2024-10-15 00:24:38 -0300

    feat: Interface otimista

--- src/api/update-profile.ts
+++ src/api/update-profile.ts
@@ -6,5 +6,9 @@ interface UpdateProfileBody {
 }
 
 export async function updateProfile({ name, description }: UpdateProfileBody) {
+  // await new Promise((_, reject) => {
+  //   setTimeout(reject, 1000)
+  // })
+
   await api.put('/profile', { name, description })
 }
