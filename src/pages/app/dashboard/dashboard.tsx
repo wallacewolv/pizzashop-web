633d41a9d295db1f6714230e3185e3fd4a21c021
wallace <wallace_wolv@hotmail.com>
2024-11-17 23:07:39 -0300

    feat: Métricas no Dashboard

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -1,7 +1,7 @@
 import { Helmet } from 'react-helmet-async'
 
 import { DayOrdersAmountCard } from './day-orders-amount-card'
-import { MonthCanceledOrdersAmountCard } from './month-canceled-orders-amounth-card'
+import { MonthCanceledOrdersAmountCard } from './month-canceled-orders-amount-card'
 import { MonthOrdersAmountCard } from './month-orders-amount-card'
 import { MonthRevenueCard } from './month-revenue-card'
 import { PopularProductsChart } from './popular-products-chart'
--- src/pages/app/dashboard/month-canceled-orders-amount-card.tsx
+++ src/pages/app/dashboard/month-canceled-orders-amount-card.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query'
-import { DollarSign } from 'lucide-react'
+import { Ban } from 'lucide-react'
 
-import { getMonthRevenue } from '../../../api/get-month-revenue'
+import { getMonthCanceledOrdersAmount } from '../../../api/get-month-canceled-orders-amount'
 import {
   Card,
   CardContent,
@@ -9,41 +9,38 @@ import {
   CardTitle,
 } from '../../../components/ui/card'
 
-export function MonthRevenueCard() {
-  const { data: monthRevenue } = useQuery({
-    queryFn: getMonthRevenue,
-    queryKey: ['metrics', 'month-revenue'],
+export function MonthCanceledOrdersAmountCard() {
+  const { data: monthCanceledOrdersAmount } = useQuery({
+    queryFn: getMonthCanceledOrdersAmount,
+    queryKey: ['metrics', 'month-canceled-orders-amount'],
   })
 
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
         <CardTitle className="text-base font-semibold">
-          Receita total (mês)
+          Cancelamentos (mês)
         </CardTitle>
-        <DollarSign className="h-4 w-4 text-muted-foreground" />
+        <Ban className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        {monthRevenue && (
+        {monthCanceledOrdersAmount && (
           <>
             <span className="text-2xl font-bold tracking-tight">
-              {(monthRevenue.receipt / 100).toLocaleString('pt-BR', {
-                style: 'currency',
-                currency: 'BRL',
-              })}
+              {monthCanceledOrdersAmount.amount.toLocaleString('pt-BR')}
             </span>
             <p className="text-xs text-muted-foreground">
-              {monthRevenue.diffFromLastMonth >= 0 ? (
+              {monthCanceledOrdersAmount.diffFromLastMonth < 0 ? (
                 <>
                   <span className="text-emerald-500 dark:text-emerald-400">
-                    +{monthRevenue.diffFromLastMonth}%
+                    {monthCanceledOrdersAmount.diffFromLastMonth}%
                   </span>{' '}
                   em relação ao mês passado
                 </>
               ) : (
                 <>
                   <span className="text-rose-500 dark:text-rose-400">
-                    {monthRevenue.diffFromLastMonth}%
+                    +{monthCanceledOrdersAmount.diffFromLastMonth}%
                   </span>{' '}
                   em relação ao mês passado
                 </>
