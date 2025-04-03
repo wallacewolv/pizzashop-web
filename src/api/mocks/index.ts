4ab2eea2a9189909bf24d9687ec786b5355c75a2
Wallace W <wallace_wolv@hotmail.com>
2025-04-03 01:11:32 -0300

    feat: Mock de perfil e atualização

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,30 +1,35 @@
-/* eslint-disable prettier/prettier */
 import { setupWorker } from 'msw/browser'
 
 import { env } from '../../env'
 import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
 import { getDaysOrdersAmountMock } from './get-day-orders-amount-mock'
+import { getManagedRestaurantMock } from './get-managed-restaurant-mock'
 import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount-mock'
 import { getMonthOrdersAmountMock } from './get-month-orders-amount-mock'
 import { getMonthRevenueMock } from './get-month-revenue-mock'
 import { getPopularProductsMock } from './get-popular-products-mock'
+import { getProfileMock } from './get-profile-mock'
 import { registerRestaurantMock } from './register-restaurant-mock'
 import { signInMock } from './sign-in-mock'
+import { updateProfileMock } from './update-profile-mock'
 
 export const worker = setupWorker(
-    signInMock,
-    registerRestaurantMock,
-    getDaysOrdersAmountMock,
-    getMonthOrdersAmountMock,
-    getMonthCanceledOrdersAmountMock,
-    getMonthRevenueMock,
-    getDailyRevenueInPeriodMock,
-    getPopularProductsMock
+  signInMock,
+  registerRestaurantMock,
+  getDaysOrdersAmountMock,
+  getMonthOrdersAmountMock,
+  getMonthCanceledOrdersAmountMock,
+  getMonthRevenueMock,
+  getDailyRevenueInPeriodMock,
+  getPopularProductsMock,
+  getProfileMock,
+  getManagedRestaurantMock,
+  updateProfileMock,
 )
 
 export async function enableMSW() {
-    if (env.MODE !== 'test') {
-        return
-    }
-    await worker.start()
+  if (env.MODE !== 'test') {
+    return
+  }
+  await worker.start()
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import { enableMSW } from './api/mocks'
-import { App } from './app'
+import { App } from './App'
 
 enableMSW().then(() => {
   createRoot(document.getElementById('root')!).render(
