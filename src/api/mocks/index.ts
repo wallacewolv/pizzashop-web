3be8a2e20552fc315c4e9a0ae1e631df5e04783d
Wallace W <wallace_wolv@hotmail.com>
2025-04-03 22:08:59 -0300

    feat: Criando mocks de ações

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,6 +1,10 @@
 import { setupWorker } from 'msw/browser'
 
 import { env } from '../../env'
+import { approveOrderMock } from './approve-order-mock'
+import { cancelOrderMock } from './cancel-order-mock'
+import { deliverOrderMock } from './deliver-order-mock'
+import { dispatchOrderMock } from './dispatch-order-mock'
 import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
 import { getDaysOrdersAmountMock } from './get-day-orders-amount-mock'
 import { getManagedRestaurantMock } from './get-managed-restaurant-mock'
@@ -29,6 +33,10 @@ export const worker = setupWorker(
   updateProfileMock,
   getOrdersMock,
   getOderDetailsMock,
+  approveOrderMock,
+  cancelOrderMock,
+  deliverOrderMock,
+  dispatchOrderMock,
 )
 
 export async function enableMSW() {
